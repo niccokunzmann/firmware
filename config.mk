87f5da9628c78577aa85621894714f252b3b659f
Sven Roederer <freifunk@it-solutions.geroedel.de>
2016-11-08 21:38:42 +0100

    config.mk: update OpenWrt base
    "patches: ar71xx: fix syntax error in /etc/uci-defaults/01_leds" has
    become upstream
    
    This reverts commit 02f8f5670cf87bbc7aab3542c2b1597467a6afd1.

--- config.mk
+++ config.mk
@@ -3,5 +3,5 @@ SHELL:=$(shell which bash)
 TARGET=ar71xx
 PACKAGES_LIST_DEFAULT=default backbone
 OPENWRT_SRC=git://github.com/openwrt/openwrt.git
-OPENWRT_COMMIT=0f757bd2606971252f901ef3faf4dbd0086315f7
+OPENWRT_COMMIT=1b6dc2e48ce654a004a7d0b98d7070a515424595
 MAKE_ARGS=

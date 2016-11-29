9eacedb0ebb277fc85e6634cc7d71fe5ff71e180
Nicco Kunzmann <niccokunzmann@rambler.ru>
2016-11-29 04:21:32 -0800

    added openwrt configuration

--- config.mk
+++ config.mk
@@ -2,6 +2,7 @@
 SHELL:=$(shell which bash)
 TARGET=ar71xx
 PACKAGES_LIST_DEFAULT=default backbone
-OPENWRT_SRC=git://github.com/openwrt/openwrt.git
-OPENWRT_COMMIT=1b6dc2e48ce654a004a7d0b98d7070a515424595
+OPENWRT_SRC=git://github.com/niccokunzmann/openwrt-zsun.git
+# branch zsun4
+OPENWRT_COMMIT=bfee5ef5cab3749860144d55512b95d74652e8a1
 MAKE_ARGS=
